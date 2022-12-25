cbed1d51a642c37c52c3e02a9199763975fc09e0
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2022-12-25 12:39:41 +0200

    push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,5 +14,20 @@ class HomeController extends Controller
     public function setImeageSlider(Request $request)
     {
         dd($request->all());
+        $event = $request->file->store('images', 'public');
+          dd( $event   );
+        $imagePath = 'storage/' . $request->inputs[0]['value'];
+
+                $filetype = exif_imagetype($imagePath);
+                dd( $filetype);
+
+                $request->inputs[0]['value']->store('images', 'public');
+        // dd();
+        // dd();
+        // dd($request->inputs[0]['value']->extension());
+        // $filename = time() . '.' . \File::extension($request->inputs[0]['value']);
+// dd( $filename);
+$request->inputs[0]['value']->move(public_path('images'), $filename);
+       dd( $filename);
     }
 }
