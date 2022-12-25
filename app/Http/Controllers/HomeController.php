42db68a3f1268368d54975663831cd9f64c25099
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2022-12-25 14:15:59 +0200

    push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -15,19 +16,23 @@ class HomeController extends Controller
     {
         dd($request->all());
         $event = $request->file->store('images', 'public');
+        DB::table('nova_settings')->insert([
+            'key ' =>$request->,
+            'value' => $event,
+
+        ]);
           dd( $event   );
         $imagePath = 'storage/' . $request->inputs[0]['value'];
 
-                $filetype = exif_imagetype($imagePath);
-                dd( $filetype);
+                // $filetype = exif_imagetype($imagePath);
+                // dd( $filetype);
 
-                $request->inputs[0]['value']->store('images', 'public');
+                // $request->inputs[0]['value']->store('images', 'public');
         // dd();
         // dd();
         // dd($request->inputs[0]['value']->extension());
         // $filename = time() . '.' . \File::extension($request->inputs[0]['value']);
 // dd( $filename);
-$request->inputs[0]['value']->move(public_path('images'), $filename);
-       dd( $filename);
+
     }
 }
